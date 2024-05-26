b3f71d45e1932434424480c9185e471f4ba5f66e
VeGaZoRo <166079069+VeGaZoRo@users.noreply.github.com>
2024-05-26 18:15:51 +0300

    Add files via upload

---
+++ VeGaMusic/utils/inline/extras.py
@@ -0,0 +1,26 @@
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+
+def botplaylist_markup(_):
+    buttons = [
+        [
+            InlineKeyboardButton(text=_["S_B_9"], url=SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+        ],
+    ]
+    return buttons
+
+
+def close_markup(_):
+    upl = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=_["CLOSE_BUTTON"],
+                    callback_data="close",
+                ),
+            ]
+        ]
+    )
+    return upl
---
+++ VeGaMusic/utils/inline/start.py
@@ -0,0 +1,48 @@
+from pyrogram.types import InlineKeyboardButton
+
+import config
+from VeGaMusic import app
+
+
+def start_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(text="المطور", user_id=config.OWNER_ID),
+          
+        ],
+         [InlineKeyboardButton(text=_["S_B_4"], callback_data="zzzback")],
+        [
+            InlineKeyboardButton(text="CHANNEL", url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text="GROUP", url=f"https://t.me/YR_HX"),
+        ],
+        [
+         
+            InlineKeyboardButton(
+                text="⭓ADD✘ME ♪",
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+    ]
+    return buttons
+
+
+def private_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(text="المطور", user_id=config.OWNER_ID),
+            
+        ],
+         [InlineKeyboardButton(text=_["S_B_4"], callback_data="zzzback")],
+        [
+            InlineKeyboardButton(text="CHANNEL", url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text="GROUP", url=f"https://t.me/YR_HX"),
+        ],
+        [
+         
+            InlineKeyboardButton(
+                text="⭓ADD✘ME ♪",
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+    ]
+    return buttons
