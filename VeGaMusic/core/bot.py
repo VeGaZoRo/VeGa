636ecad07ce3d2bd863d7cf3652578e92688d272
VeGaZoRo <166079069+VeGaZoRo@users.noreply.github.com>
2024-05-26 18:02:28 +0300

    Add files via upload

---
+++ VeGaMusic/core/bot.py
@@ -0,0 +1,54 @@
+from pyrogram import Client, errors
+from pyrogram.enums import ChatMemberStatus, ParseMode
+
+import config
+
+from ..logging import LOGGER
+
+
+class Zoro(Client):
+    def __init__(self):
+        LOGGER("ميــوزك فيجا").info(f"جارِ بدء تشغيل البوت . . .")
+        super().__init__(
+            name="VeGaMusic",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            bot_token=config.BOT_TOKEN,
+            in_memory=True,
+            parse_mode=ParseMode.HTML,
+            max_concurrent_transmissions=7,
+        )
+
+    async def start(self):
+        await super().start()
+        self.id = self.me.id
+        self.name = self.me.first_name + " " + (self.me.last_name or "")
+        self.username = self.me.username
+        self.mention = self.me.mention
+
+        try:
+            await self.send_message(
+                chat_id=config.LOGGER_ID,
+                text=f"<u><b>» تم تشغيل الميـوزك لـ البوت {self.mention} :</b><u>\n\n- ɪᴅ : <code>{self.id}</code>\n- ɴᴀᴍᴇ : {self.name}\n- ᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
+            )
+        except (errors.ChannelInvalid, errors.PeerIdInvalid):
+            LOGGER(__name__).error(
+                "» قم باضافة البـوت مشـرفـاً بكافة الصلاحيات في مجموعـة السجـل"
+            )
+            exit()
+        except Exception as ex:
+            LOGGER(__name__).error(
+                f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).__name__}."
+            )
+            exit()
+
+        a = await self.get_chat_member(config.LOGGER_ID, self.id)
+        if a.status != ChatMemberStatus.ADMINISTRATOR:
+            LOGGER(__name__).error(
+                "» قم برفـع البـوت مشـرفـاً بكافة الصلاحيات في مجموعـة السجـل"
+            )
+            exit()
+        LOGGER("ميــوزك فيجا").info(f" تم بدء تشغيل البوت {self.name} ...✓")
+
+    async def stop(self):
+        await super().stop()
---
+++ VeGaMusic/core/mongo.py
@@ -0,0 +1,14 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from config import MONGO_DB_URI
+
+from ..logging import LOGGER
+
+LOGGER("ميــوزك فيجا").info("جـارِ الاتصـال بقاعـدة البيانـات . . .")
+try:
+    _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
+    mongodb = _mongo_async_.Anon
+    LOGGER("ميــوزك فيجا").info("تم الاتصـال بقاعـدة البيانـات ...✓")
+except:
+    LOGGER(__name__).error("حدث خطأ اثناء الاتصال بقاعدة البيانات.")
+    exit()
