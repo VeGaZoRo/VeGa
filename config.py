2423ce60f859465010901ccc907a49e63e017740
VeGaZoRo <166079069+VeGaZoRo@users.noreply.github.com>
2024-05-27 00:13:43 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://proceed58:proceed58@cluster
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 2000))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", None))
+LOGGER_ID = int(getenv("OWNER_ID","1872738364"))
 
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 6753126490))
