998ead4c772de93b3a7b68cbf6a1313df929ab38
VeGaZoRo <166079069+VeGaZoRo@users.noreply.github.com>
2024-05-26 20:12:26 +0300

    Add files via upload

--- VeGaMusic/core/call.py
+++ VeGaMusic/core/call.py
@@ -556,7 +556,7 @@ class Call(PyTgCalls):
         return str(round(sum(pings) / len(pings), 3))
 
     async def start(self):
-        LOGGER("ميــوزك عفرتو").info("جارِ تحميل مكاتب الميوزك . . .\n")
+        LOGGER("ميــوزك فيجا").info("جارِ تحميل مكاتب الميوزك . . .\n")
         if config.STRING1:
             await self.one.start()
         if config.STRING2:
