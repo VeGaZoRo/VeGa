e30f805f4aab6f2ae7752d5cd9be4a55298feb88
VeGaZoRo <166079069+VeGaZoRo@users.noreply.github.com>
2024-05-26 18:13:55 +0300

    Add files via upload

---
+++ VeGaMusic/utils/inlinequery.py
@@ -0,0 +1,44 @@
+from pyrogram.types import InlineQueryResultArticle, InputTextMessageContent
+
+answer = []
+
+answer.extend(
+    [
+        InlineQueryResultArticle(
+            title="ايقاف",
+            description=f"ᴩᴀᴜsᴇ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+            thumb_url="https://telegra.ph/file/c5952790fa8235f499749.jpg",
+            input_message_content=InputTextMessageContent("قف"),
+        ),
+        InlineQueryResultArticle(
+            title="إكمال",
+            description=f"ʀᴇsᴜᴍᴇ ᴛʜᴇ ᴩᴀᴜsᴇᴅ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+            thumb_url="https://telegra.ph/file/c5952790fa8235f499749.jpg",
+            input_message_content=InputTextMessageContent("كمل"),
+        ),
+        InlineQueryResultArticle(
+            title="تخطي",
+            description=f"sᴋɪᴩ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀɴᴅ ᴍᴏᴠᴇs ᴛᴏ ᴛʜᴇ ɴᴇxᴛ sᴛʀᴇᴀᴍ.",
+            thumb_url="https://telegra.ph/file/c5952790fa8235f499749.jpg",
+            input_message_content=InputTextMessageContent("تخطي"),
+        ),
+        InlineQueryResultArticle(
+            title="انهاء",
+            description="ᴇɴᴅ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+            thumb_url="https://telegra.ph/file/c5952790fa8235f499749.jpg",
+            input_message_content=InputTextMessageContent("انهاء"),
+        ),
+        InlineQueryResultArticle(
+            title="Sʜᴜғғʟᴇ",
+            description="sʜᴜғғʟᴇ ᴛʜᴇ ǫᴜᴇᴜᴇᴅ sᴏɴɢs ɪɴ ᴩʟᴀʏʟɪsᴛ.",
+            thumb_url="https://telegra.ph/file/c5952790fa8235f499749.jpg",
+            input_message_content=InputTextMessageContent("/shuffle"),
+        ),
+        InlineQueryResultArticle(
+            title="لوب",
+            description="ʟᴏᴏᴩ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ ᴛʀᴀᴄᴋ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+            thumb_url="https://telegra.ph/file/c5952790fa8235f499749.jpg",
+            input_message_content=InputTextMessageContent("لوب 3"),
+        ),
+    ]
+)
---
+++ VeGaMusic/utils/logger.py
@@ -0,0 +1,37 @@
+from pyrogram.enums import ParseMode
+
+from VeGaMusic import app
+from VeGaMusic.utils.database import is_on_off
+from config import LOGGER_ID
+
+
+async def play_logs(message, streamtype):
+    if await is_on_off(2):
+        logger_text = f"""
+ٴ<b>•────‌‌‏──‌‌‏─‌‌‏─‌‌‏─‌‌‏─‌‌‏─‌‌‏─•</b>
+<b>إشعـارات  الميـوزك 𝄞</b>
+ٴ<b>•────‌‌‏─‌‌‏─‌‌‏─‌‌‏─‌‌‏──‌‌‏─‌‌‏─•</b>
+<b>- سيـدي المطـور</b>
+<b>- هنـاك شخص يستخـدم الميـوزك حاليـاً</b>
+
+<b>- الاسم :</b> {message.from_user.mention}
+<b>- اليوزر :</b> @{message.from_user.username}
+<b>- ايدي المستخدم :</b> <code>{message.from_user.id}</code>
+
+<b>- اسم المجموعة :</b> {message.chat.title}
+<b>- يوزر المجموعة :</b> @{message.chat.username}
+<b>- ايدي المجموعة :</b> <code>{message.chat.id}</code>
+
+<b>- الطلب :</b> {message.text.split(None, 1)[1]}
+<b>- نوع التشغيل :</b> {streamtype}"""
+        if message.chat.id != LOGGER_ID:
+            try:
+                await app.send_message(
+                    chat_id=LOGGER_ID,
+                    text=logger_text,
+                    parse_mode=ParseMode.HTML,
+                    disable_web_page_preview=True,
+                )
+            except:
+                pass
+        return
