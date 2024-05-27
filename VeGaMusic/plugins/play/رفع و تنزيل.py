d33408b39a5b69b7dbb59947cdb09428cacf0409
VeGaZoRo <166079069+VeGaZoRo@users.noreply.github.com>
2024-05-27 17:48:50 +0300

    Add files via upload

--- VeGaMusic/plugins/play/رفع و تنزيل.py
+++ VeGaMusic/plugins/play/رفع و تنزيل.py
@@ -236,7 +236,7 @@ async def mallekandv(client, message):
         
         
         
-@app.on_message(command(['رتبتي'][]), group=2889933100)
+@app.on_message(command(['رتبتي']), group=2889933100)
 async def ororhe(client: Client, message: Message):
     me = await client.get_me()
     bot_username = me.username
