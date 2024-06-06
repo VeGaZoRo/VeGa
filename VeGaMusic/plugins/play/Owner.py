79c36850fd82088aae830c39ed9709ce05649fba
ZoRo VeGa <166079069+VeGaZoRo@users.noreply.github.com>
2024-06-07 02:14:02 +0300

    Update Owner.py

--- VeGaMusic/plugins/play/Owner.py
+++ VeGaMusic/plugins/play/Owner.py
@@ -19,7 +19,7 @@ async def vgdg(client: Client, message: Message):
       else:
             chat_id = message.chat.id
             f = "administrators"
-            async for member in client.iter_chat_members(chat_id, filter=f):
+            async for member in client.get_chat_members(chat_id, filter=f):
                if member.status == "creator":
                  id = member.user.id
                  key = InlineKeyboardMarkup([[InlineKeyboardButton(member.user.first_name, user_id=id)]])
