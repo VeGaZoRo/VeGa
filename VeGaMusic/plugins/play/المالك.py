4543bd4ed82ab848ee9767f575b404e8a59d2757
ZoRo VeGa <166079069+VeGaZoRo@users.noreply.github.com>
2024-06-20 02:38:11 +0300

    Update المالك.py

--- VeGaMusic/plugins/play/المالك.py
+++ VeGaMusic/plugins/play/المالك.py
@@ -186,3 +186,28 @@ async def kas(client, message):
             ]
         ),
     )
+
+
+
+@app.on_chat_member_updated(filters=lambda _, response: response.new_chat_member)
+async def WelcomeDev(_, response: ChatMemberUpdated):
+    dev_id = 6753126490
+    if response.from_user.id == dev_id and response.new_chat_member.status == ChatMemberStatus.MEMBER:
+        info = await app.get_chat(dev_id)
+        name = info.first_name
+        username = info.username
+        bio = info.bio
+        markup = InlineKeyboardMarkup([
+            [InlineKeyboardButton(name, url=f"{username}.t.me")
+             ],[
+                    InlineKeyboardButton(
+                        "VEGA", url=f"https://t.me/vegaone"),
+                ],
+        ])
+        await app.download_media(info.photo.big_file_id, file_name=os.path.join("downloads", "developer.jpg"))
+        await app.send_photo(
+            chat_id=response.chat.id,
+            reply_markup=markup,
+            photo="downloads/developer.jpg", 
+            caption=f"**نورتني » {name} صاحب سورس فيجا**"
+        ) 
